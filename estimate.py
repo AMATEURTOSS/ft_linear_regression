9406bab53380859e773bed002906b31086f5185a
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-02-22 17:27:50 +0900

    [ADD] add normalization

--- estimate.py
+++ estimate.py
@@ -2,7 +2,7 @@ import csv
 from sys import argv
 
 
-def get_theta() -> list[int]:
+def get_data() -> list[int]:
     ret = [0, 0]
     try:
         theta_file = open("theta.txt", "r")
@@ -13,7 +13,25 @@ def get_theta() -> list[int]:
     return ret
 
 
+def original_data_to_normalized_data(target, max_val, min_val):
+    return (target - min_val) / (max_val - min_val)
+
+
+def normalized_data_to_original_data(target, max_val, min_val):
+    return target * (max_val + min_val) + min_val
+
+
 if __name__ == '__main__':
-    theta = get_theta()
+    data = get_data()
+    normalized_theta0 = float(data[0])
+    normalized_theta1 = float(data[1])
+    mileage_max = float(data[2])
+    mileage_min = float(data[3])
+    price_max = float(data[4])
+    price_min = float(data[5])
     mileage = int(argv[1])
-    print(f"Estimated price is {float(theta[0]) + float(theta[1]) * mileage}")
+    normalized_estimated_price = float(normalized_theta0) + float(normalized_theta1) \
+                                 * original_data_to_normalized_data(mileage, mileage_max, mileage_min)
+    print(f"normalized price: {normalized_estimated_price}")
+    estimated_price = normalized_data_to_original_data(normalized_estimated_price, price_max, price_min)
+    print(f"Estimated price is {estimated_price}")
--- trainer.py
+++ trainer.py
@@ -19,55 +19,74 @@ def is_valid_parameter():
 class Trainer:
     theta: list = [0.0, 0.0]
     data: list = []
+    normalized_data: list = []
+    mileage_max: int
+    mileage_min: int
+    price_max: int
+    price_min: int
 
     def __init__(self, data_file_name: str):
         data_file = open(data_file_name, "r")
         reader = csv.reader(data_file)
         for mileage, price in reader:
             self.data.append({"mileage": int(mileage), "price": int(price)})
-            plt.scatter(int(mileage), int(price))
+        self.normalization()
         data_file.close()
+
+    def normalization(self) -> None:
+        self.mileage_max = max(self.data, key=lambda el: el["mileage"])["mileage"]
+        self.mileage_min = min(self.data, key=lambda el: el["mileage"])["mileage"]
+        self.price_max = max(self.data, key=lambda el: el["price"])["price"]
+        self.price_min = min(self.data, key=lambda el: el["price"])["price"]
+        for element in self.data:
+            mileage = element["mileage"]
+            price = element["price"]
+            normalized_mileage = (mileage - self.mileage_min) / (self.mileage_max - self.mileage_min)
+            normalized_price = (price - self.price_min) / (self.price_max - self.price_min)
+            self.normalized_data.append({"mileage": normalized_mileage, "price": normalized_price})
+            plt.scatter(normalized_mileage, normalized_price)
 
     def get_estimate_price(self, mileage: int):
         return self.theta[0] + self.theta[1] * mileage
 
     def calc_theta_0(self) -> float:
         new_theta = 0
-        for element in self.data:
-            mileage = int(element["mileage"])
-            price = int(element["price"])
+        for element in self.normalized_data:
+            mileage = element["mileage"]
+            price = element["price"]
             new_theta += (self.get_estimate_price(mileage) - price)
-        new_theta = ((new_theta / len(self.data)) * 0.01)
+        new_theta = ((new_theta / len(self.normalized_data)) * 0.1)
         return new_theta
 
     def calc_theta_1(self) -> float:
         new_theta = 0
-        for element in self.data:
-            mileage = int(element["mileage"])
-            price = int(element["price"])
+        for element in self.normalized_data:
+            mileage = element["mileage"]
+            price = element["price"]
             new_theta += ((self.get_estimate_price(mileage) - price) * mileage)
-        new_theta = ((new_theta / len(self.data)) * 0.00000000015)
+        new_theta = ((new_theta / len(self.normalized_data)) * 0.1)
         return new_theta
 
     def calc_cost(self) -> int:
         cost_sum = 0
-        for element in self.data:
+        for element in self.normalized_data:
             mileage = int(element["mileage"])
             price = int(element["price"])
-            cost_sum += ((self.get_estimate_price(mileage) - price) ** 2) / len(self.data)
+            cost_sum += ((self.get_estimate_price(mileage) - price) ** 2) / len(self.normalized_data)
         return cost_sum
 
     def write_to_file(self) -> None:
         theta_file = open("theta.txt", "w+")
-        theta_file.write(f"{self.theta[0]},{self.theta[1]}")
+        theta_file.write(f"{self.theta[0]},{self.theta[1]},{self.mileage_max},{self.mileage_min},{self.price_max},{self.price_min}")
 
     def training(self):
         for i in range(10000):
             self.theta[0] -= self.calc_theta_0()  # b - b_grad
             self.theta[1] -= self.calc_theta_1()  # w - w_grad
-            print("{0:2} w = {1:.5f}, b = {2:.5f} error = {3:.5f}"
-                  .format(i, self.theta[1], self.theta[0], self.calc_cost()))
-        plt.plot([0, 250000], [self.get_estimate_price(0), self.get_estimate_price(250000)])
+        print(f"w: {self.theta[1]}")
+        print(f"b: {self.theta[0]}")
+        print(f"e: {self.calc_cost()}")
+        plt.plot([0, 1], [self.get_estimate_price(0), self.get_estimate_price(1)])  # draw line on graph
         self.write_to_file()
 
 
