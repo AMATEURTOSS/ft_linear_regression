6370329ab9c1c7dc645695b527520e7562a55d7f
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-02-24 16:37:10 +0900

    [ADD] add option prototype

--- trainer.py
+++ trainer.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 
 
 def is_valid_parameter():
-    if len(sys.argv) != 2:
+    if (len(sys.argv) != 2 and len(sys.argv) != 3) or\
+            (len(sys.argv) == 3 and sys.argv[2] != "-cw" and sys.argv[2] != "-cb"):
         print("ERROR: Invalid parameter")
-        print("USAGE: python trainer.py [file_name]")
+        print("USAGE: python trainer.py <file_name> [-option]")
+        print("OPTIONS:")
+        print("        -cw: graph the cost for w(gradient")
+        print("        -cb: graph the cost for b(y-intercept")
         exit(1)
     try:
         file = open(sys.argv[1], "r")
@@ -16,22 +20,30 @@ def is_valid_parameter():
         exit(1)
 
 
+def read_data() -> list:
+    data = []
+    data_file = open(file_name, "r")
+    reader = csv.reader(data_file)
+    for mileage, price in reader:
+        data.append({"mileage": int(mileage), "price": int(price)})
+    data_file.close()
+    return data
+
+
 class Trainer:
     __theta: list = [0.0, 0.0]
-    __data: list = []
+    __data: list
+    __option: str
     __normalized_data: list = []
     __mileage_max: int
     __mileage_min: int
     __price_max: int
     __price_min: int
 
-    def __init__(self, data_file_name: str):
-        data_file = open(data_file_name, "r")
-        reader = csv.reader(data_file)
-        for mileage, price in reader:
-            self.__data.append({"mileage": int(mileage), "price": int(price)})
+    def __init__(self, data: list, option: str):
+        self.__data = data
+        self.__option = option
         self.__normalization()
-        data_file.close()
 
     def __normalization(self) -> None:
         self.__mileage_max = max(self.__data, key=lambda el: el["mileage"])["mileage"]
@@ -44,7 +56,8 @@ class Trainer:
             normalized_mileage = (mileage - self.__mileage_min) / (self.__mileage_max - self.__mileage_min)
             normalized_price = (price - self.__price_min) / (self.__price_max - self.__price_min)
             self.__normalized_data.append({"mileage": normalized_mileage, "price": normalized_price})
-            plt.scatter(normalized_mileage, normalized_price)
+            if self.__option is None:
+                plt.scatter(normalized_mileage, normalized_price)
 
     def __get_estimate_price(self, mileage: int):
         return self.__theta[0] + self.__theta[1] * mileage
@@ -86,17 +99,24 @@ class Trainer:
 
     def training(self):
         for i in range(10000):
-            self.__theta[0] -= self.__calc_theta_0()  # b - b_grad
-            self.__theta[1] -= self.__calc_theta_1()  # w - w_grad
-            print(f"e: {self.__calc_cost()}")
+            if self.__option == "-cb" or self.__option is None:
+                self.__theta[0] -= self.__calc_theta_0()  # b - b_grad
+            if self.__option == "-cw" or self.__option is None:
+                self.__theta[1] -= self.__calc_theta_1()  # w - w_grad
+            if self.__option is not None:
+                plt.scatter(self.__theta[1], self.__calc_cost())
         print(f"w: {self.__theta[1]}")
         print(f"b: {self.__theta[0]}")
-        plt.plot([0, 1], [self.__get_estimate_price(0), self.__get_estimate_price(1)])
+        if self.__option is None:
+            plt.plot([0, 1], [self.__get_estimate_price(0), self.__get_estimate_price(1)])
         self.__write_to_file()
 
 
 if __name__ == '__main__':
     is_valid_parameter()
-    tr = Trainer(sys.argv[1])
+    file_name = sys.argv[1]
+    option = sys.argv[2] if len(sys.argv) == 3 else None
+    data = read_data()
+    tr = Trainer(data, option)
     tr.training()
     plt.show()
